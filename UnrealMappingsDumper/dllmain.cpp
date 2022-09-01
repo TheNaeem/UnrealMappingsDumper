3a54f574f1aaade497b52fa380d004f208047ea8
shade <nkanaan1027@gmail.com>
2022-09-01 16:31:31 -0400

    Add project files.

---
+++ UnrealMappingsDumper/dllmain.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+
+void WINAPI Main()
+{
+	
+}
+
+BOOL APIENTRY DllMain(
+	HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)Main, hModule, 0, nullptr));
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
